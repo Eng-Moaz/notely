585947687345918a260c5d5ad597b99b0c1cf029
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-07-23 17:18:20 +0300

    Added code formatting to the CI

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -5,40 +5,40 @@ import (
 	"testing"
 )
 
-type headerTest struct{
+type headerTest struct {
 	header http.Header
-	key string
-	value string
-	pass bool
+	key    string
+	value  string
+	pass   bool
 }
 
-func (h *headerTest) setHeaders(){
+func (h *headerTest) setHeaders() {
 	h.header.Set(h.key, h.value)
 }
 
-func Test_APIKey(t *testing.T){
+func Test_APIKey(t *testing.T) {
 	headers := []*headerTest{
 		{
 			header: http.Header{},
-			key: "Authorization",
-			value: "ApiKey someApiKey",
-			pass: true,
+			key:    "Authorization",
+			value:  "ApiKey someApiKey",
+			pass:   true,
 		},
 		{
 			header: http.Header{},
-			key: "Something else",
-			value: "ApiKey someApiKey",
-			pass: false,
+			key:    "Something else",
+			value:  "ApiKey someApiKey",
+			pass:   false,
 		},
 	}
 
-	for _, header := range headers{
+	for _, header := range headers {
 		header.setHeaders()
-		_, err := GetAPIKey(header.header)	
-		if err == nil && header.pass == false{
+		_, err := GetAPIKey(header.header)
+		if err == nil && header.pass == false {
 			t.Error("Something is wrong")
-		}else if err != nil && header.pass == true{
+		} else if err != nil && header.pass == true {
 			t.Error("Something is wrong")
 		}
-	}	
+	}
 }
