120606b17671c664b51c14f541934444838423a3
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-07-24 16:14:53 +0300

    Added a linter to the CI pipeline

--- main.go
+++ main.go
@@ -24,7 +24,12 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
+func unused(){
+	//literally does nothing
+}
+
 func main() {
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
