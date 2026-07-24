d5443c936410ba3e58349e2df3b4477651f5222f
Eng-Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-07-24 16:34:22 +0300

    Fixed security issuses

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -92,8 +93,9 @@ func main() {
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: router,
+		ReadHeaderTimeout: 3 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	// log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
